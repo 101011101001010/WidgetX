fdf332de2b4c61e8b61b8b225ff9bdaf97f61e9f
Teo Wei Jie <wjayteo@gmail.com>
2021-07-22 01:16:36 +0800

    Update divider

--- lib/basic/divider.dart
+++ lib/basic/divider.dart
@@ -8,7 +8,7 @@ class DividerX extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Color color = ColorX.get(context, ColorXType.DIVIDER);
-    if (vertical) return VerticalDivider(width: 1.2, thickness: 1.2, color: color);
+    if (vertical) return VerticalDivider(width: 0.6, thickness: 0.6, color: color);
     return Divider(height: 0, thickness: 0.5, color: color);
   }
 }
